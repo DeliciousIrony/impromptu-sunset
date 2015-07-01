93a8975f1a710c4c2275fc74fdfae0d467432b69
Niall O'Kane <okanenm@gmail.com>
2015-07-01 12:24:47 -0700

    Implemented renderProfile() in UsersController

--- client/app/users/UsersController.js
+++ client/app/users/UsersController.js
@@ -1,10 +1,18 @@
 
-app.controller('UsersController', ['$scope', 'Users',  function($scope, Users) {
+app.controller('UsersController', ['$scope', 'Users', 'Sessions', 'Profiles', function($scope, Users, Sessions, Profiles) {
 
   $scope.users = {};
   $scope.users.userList = [];
   Users.allUsers().then(function(res) { 
     $scope.users.userList = res.data.userData;
+    console.log($scope.users.userList);
   });
+
+  $scope.renderProfile = function(userId) {
+    console.log('RENDER THIS PROFILE ID', userId);
+    Profiles.setProfileId(userId);
+  }
+
+  
 
 }]);
