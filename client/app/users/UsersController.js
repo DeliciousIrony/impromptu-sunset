d932f8e00fffe7867b15f091cb3bdd1deb31b961
Niall O'Kane <okanenm@gmail.com>
2015-06-30 20:17:35 -0700

    Setting userList to the retrieved list of users from PostgreSQL

--- client/app/users/UsersController.js
+++ client/app/users/UsersController.js
@@ -1,6 +1,10 @@
 
-app.controller('UsersController', ['$scope', function($scope) {
+app.controller('UsersController', ['$scope', 'Users',  function($scope, Users) {
 
-
+  $scope.users = {};
+  $scope.users.userList = [];
+  Users.allUsers().then(function(res) { 
+    $scope.users.userList = res.data.userData;
+  });
 
 }]);
