1b00c6efcb30d1e038bfdcea25a060f66ef21e9c
Niall O'Kane <okanenm@gmail.com>
2015-07-01 17:15:37 -0700

    Created 'Comment' Sequelize model

---
+++ server/models/comment.js
@@ -0,0 +1,18 @@
+// The Comment model
+// This is auto generated using the sequelize-cli command 
+// 'sequelize model:create'
+module.exports = function(sequelize, DataTypes) {
+  var Comment = sequelize.define('Comment', {
+    text: DataTypes.TEXT,
+    userId: DataTypes.INTEGER,
+    sessionId: DataTypes.INTEGER
+  }, {
+    classMethods: {
+      associate: function(models) {
+         Comment.belongsTo(models.User);
+         Comment.belongsTo(models.Session);
+      }
+    }
+  });
+  return Comment;
+};
