fe766fd233d16798dddb289debb3eedfee16037d
Niall O'Kane <okanenm@gmail.com>
2015-07-01 21:18:34 -0700

    (refactor) refactored newComment()

--- server/controllers/commentController.js
+++ server/controllers/commentController.js
@@ -14,31 +14,28 @@ module.exports = {
     db.User.findById(userId).then(function(user){
       // once retrieved, create a comment
       db.Session.findById(sessionId).then(function(session){
-        db.Comment.build({
+        var c = db.Comment.build({
           text: text, 
           userId: userId,
           sessionId: sessionId,
-        }).save().then(function(c){
-          // add that comment to the list of user comments
-          // since a user has many comments (1 to many relationship)
-          user.addComments(c).then(function(x){
-          // add that comment to the list of session comments
-          // since a user has many comments (1 to many relationship)
-            session.addComments(c).then(function(y){
-              res.status(201).send("Comment Created");
-            }).catch(function(err){
-              console.log("Error adding comment to the Session")
-              console.log(err);
-              res.status(422).send(err);
-            });
+        });
+        // add that comment to the list of user comments
+        // since a user has many comments (1 to many relationship)
+        user.addComments(c).then(function(x){
+        // add that comment to the list of session comments
+        // since a user has many comments (1 to many relationship)
+          session.addComments(c).then(function(y){
+            res.status(201).send("Comment Created");
           }).catch(function(err){
-            console.log("Error adding comment to the User");
+            console.log("Error adding comment to the Session")
             console.log(err);
             res.status(422).send(err);
-          });  
-        });        
-
-        
+          });
+        }).catch(function(err){
+          console.log("Error adding comment to the User");
+          console.log(err);
+          res.status(422).send(err);
+        });
       });
     });
 
